09091a003dfb49c5c0a76341e011a79f154d0d83
Michael Remington <53287026+michaelremington2@users.noreply.github.com>
2023-12-05 12:24:49 -0800

    Add files via upload

---
+++ tidy_intro.R
@@ -0,0 +1,28 @@
+library(datasets)
+library(tidyverse)
+
+##############
+# Load Data ##
+##############
+
+# This is a built in R data set. 
+# After running this command you will have a dataframe called iris
+data(iris)
+
+##############
+# Challenges #
+##############
+
+# Look at the data
+
+# Get some summary statistics on your data
+
+# make a histogram of sepal length
+
+# capitalize all the species names
+
+# make a visualization of sepal length to width with dots colored by species
+
+# make a new column that's called sepal area that is sepal length times sepal width
+
+# visualize sepal area by species
